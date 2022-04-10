b484d3f2bee083673fa2e2692debef305fcc3278
Abdo-ME <abdelhakmehda@gmail.com>
2022-04-10 23:48:03 +0100

    handle cart comp

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,17 +1,18 @@
-import { CART_ADD_ITEM } from "./types"
+// import axios from "axios"
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "./types"
 
-export const addToCartItem = (product, qty) => async (dispatch, getState) => {
-    const cartItemsClone =   getState().cart.cartItems;
+export const addToCartItem = (product, qty) =>  (dispatch, getState) => {
+    // const product = await axios.get(`/api/products/${product._id}`)
+    const cartItemsClone = getState().cart.cartItems;
     
     try {
         const productClone =  {...product}
-    
         const isExist = cartItemsClone.some(item => {
             return item._id === product._id
         })
         if (isExist) {
             const productFound= cartItemsClone.find(p=>p._id===product._id)
-            productFound.qty = productFound.qty + +qty
+            productFound.qty =  +qty
         } else {
             productClone.qty= +qty
             cartItemsClone.push(productClone)
@@ -27,4 +28,16 @@ export const addToCartItem = (product, qty) => async (dispatch, getState) => {
         console.log(error);
         
     }
+}
+
+
+//Remove a Product
+export const removeFromCartItem = (id) => (dispatch, getState) => {
+    const cartItems = getState().cart.cartItems;
+    const newCartItems = cartItems.filter(p => id !== p._id)
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: newCartItems
+    })
+    localStorage.setItem('cartItems',JSON.stringify(newCartItems))
 }
