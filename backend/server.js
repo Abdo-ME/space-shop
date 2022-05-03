652cbf0cc39e63b2784c8d9da462a7403885f57f
Abdo-ME <abdelhakmehda@gmail.com>
2022-05-03 11:46:50 +0100

    add upload image functionality to the backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import  express  from "express"
+import path from 'path'
+import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
 import connectDB from "./config/db.js"
@@ -6,6 +7,7 @@ import { notFoundPage,errorHandler } from "./middleware/errorMiddleWare.js"
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import orderRoutes from "./routes/orderRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
 
 
 dotenv.config()
@@ -25,8 +27,11 @@ app.use('/api/products/', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/config/paypal', (req,res)=> res.send(process.env.PAYPAL_CLIENT_ID));
+app.use('/api/upload', uploadRoutes);
 
-
+// make upload file static
+const __dirname = path.resolve()
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
 
 //handle 404 error (not found Page)
